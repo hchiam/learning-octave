a5a4dfb025df9c4d4a488fe8235889c7a759e973
hchiam <hchiam@users.noreply.github.com>
2022-02-22 22:41:56 -0500

    get sigmoid function to work element-wise too

--- ml/sigmoid.m
+++ ml/sigmoid.m
@@ -1,6 +1,6 @@
-% you can use this in CLI by going to the right directory, run octave, and running sigmoid(1)
+% you can use this in CLI by going to the right directory, run octave, and running sigmoid(1) or sigmoid([1,2,3])
 function g = sigmoid(z)
-  g = 1 / ( 1 + e ^ (-z) ) % function will return value of g
-  % "sigmoid = ..." will not work here, but sigmoid(1) will work in CLI to get output
-  return;
+  g = 1 ./ ( 1 + e .^ (-z) ) % function will return value of g
+  % "sigmoid = ..." will not work here, 
+  % but sigmoid(1) or sigmoid([1,2,3]) will work in CLI to get output
 end
